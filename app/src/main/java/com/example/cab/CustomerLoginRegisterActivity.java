e8d0e0d8202c2da8bee4cd25dd215ab8e943b27d
minhazv14 <47391006+minhazv14@users.noreply.github.com>
2020-09-08 17:52:03 +0600

    changes name

---
+++ app/src/main/java/com/example/cab/CustomerLoginRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.cab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerLoginRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_login_register);
+    }
+}
