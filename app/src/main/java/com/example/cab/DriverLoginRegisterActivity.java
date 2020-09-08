bce604682275161dfd3cd39c8b03965047f4d9c0
minhazv14 <47391006+minhazv14@users.noreply.github.com>
2020-09-08 17:48:28 +0600

    changes name

---
+++ app/src/main/java/com/example/cab/DriverLoginRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.cab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverLoginRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_login_register);
+    }
+}
